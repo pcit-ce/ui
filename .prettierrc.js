868d8aac5dae115db15e13f98b357e12b9a30d95
khs1994 <khs1994@khs1994.com>
2018-11-27 09:31:05 +0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+'use strict';
+// https://prettier.io/docs/en/configuration.html
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  useTabs: false,
+  trailingComma: 'all',
+};
---
+++ js/common/util.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ucfirst(word) {
+    return word.substring(0, 1).toUpperCase() + word.substring(1);
+  },
+};
